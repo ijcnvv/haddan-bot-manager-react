62f7ffe363032fa06bec7d82cad720961199cf17
Vadim Novikov <ijcnvv@gmail.com>
2020-03-20 11:09:28 +0300

    adds cash popup

--- src/pages/user/popups/modules/edit/modules/cash.js
+++ src/pages/user/popups/modules/edit/modules/cash.js
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CashPopup = props => {
+  const [path, setPath] = useState(null);
+  const defaultValue = 300;
+  const title = `+ ${defaultValue} руб`;
+
   console.log(props);
   return <></>;
 };
