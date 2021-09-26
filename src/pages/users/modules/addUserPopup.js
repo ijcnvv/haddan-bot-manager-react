66cdea4a317f422697c28e3ec436679c3aa3523f
Vadim Novikov <ijcnvv@gmail.com>
2021-09-26 13:14:00 +0300

    removes email

--- src/pages/users/modules/addUserPopup.js
+++ src/pages/users/modules/addUserPopup.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { useForm, Controller } from "react-hook-form";
-import { fetchAddUser } from "actions/usersActions";
-import PropTypes from "prop-types";
-import Popup from "components/shared/popup/popup";
-import Loader from "components/shared/loader/loader";
-import Select from "components/shared/select/select";
-import Input from "components/shared/input/input";
-import Button from "components/shared/button/button";
-import "./addUserPopup.scss";
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useForm, Controller } from 'react-hook-form';
+import { fetchAddUser } from 'actions/usersActions';
+import PropTypes from 'prop-types';
+import Popup from 'components/shared/popup/popup';
+import Loader from 'components/shared/loader/loader';
+import Select from 'components/shared/select/select';
+import Input from 'components/shared/input/input';
+import Button from 'components/shared/button/button';
+import './addUserPopup.scss';
 
 const AddUser = ({ closePopup }) => {
   const dispatch = useDispatch();
@@ -17,12 +17,12 @@ const AddUser = ({ closePopup }) => {
   const { register, handleSubmit, errors, control } = useForm();
   const { loading, users } = useSelector(({ users }) => users);
   const networkList = {
-    telegram: "Telegram",
-    skype: "Skype",
-    whatsapp: "WhatsApp",
+    telegram: 'Telegram',
+    skype: 'Skype',
+    whatsapp: 'WhatsApp',
   };
   const usersList = users.map(({ id, name }) => ({ value: id, text: name }));
-  const submitClassName = `btn${loading ? " disabled" : ""}`;
+  const submitClassName = `btn${loading ? ' disabled' : ''}`;
   const onSuccess = (id) => history.push(`/users/${id}`);
   const onFormSubmit = (data) => dispatch(fetchAddUser(data, onSuccess));
 
@@ -37,23 +37,15 @@ const AddUser = ({ closePopup }) => {
               className="col s12"
               placeholder="Имя пользователя"
               name="name"
-              register={register({ required: "Name is required" })}
+              register={register({ required: 'Name is required' })}
               error={errors.name}
-            />
-            <Input
-              className="col s12"
-              placeholder="Email"
-              type="email"
-              name="email"
-              register={register({ required: "Email is required" })}
-              error={errors.email}
             />
             <Input
               type="number"
               className="col s6"
               placeholder="ID персонажа"
               name="player_id"
-              register={register({ required: "ID is required" })}
+              register={register({ required: 'ID is required' })}
               error={errors.player_id}
             />
             <Controller
