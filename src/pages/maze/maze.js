6376e3d5ed34a48f274f29e0e2fbd93c70bca0c0
Vadim Novikov <ijcnvv@gmail.com>
2022-01-01 14:02:44 +0300

    implements changing query params on maze page

--- src/pages/maze/maze.js
+++ src/pages/maze/maze.js
@@ -1,31 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useLocation, useHistory } from 'react-router-dom';
 import { fetchMaze } from '../../actions/mazeActions';
 import Select from 'components/shared/select/select';
 import Loader from 'components/shared/loader/loader';
 import './maze.scss';
 
 const MazePage = () => {
-  const dispatch = useDispatch();
-  const { data, loading } = useSelector(({ maze }) => maze);
-  const [lvl, setLevel] = useState('1');
+  const initLevel = '1';
+  const optionsList = { 1: 'I Уровень', 2: 'II Уровень', 3: 'III Уровень' };
 
-  const createMaze = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const { search } = useLocation();
+  const { data, loading } = useSelector(({ maze }) => maze);
+  const [lvl, setLevel] = useState(initLevel);
+  const query = useMemo(() => new URLSearchParams(search), [search]);
+
+  const getMazeHtml = () => {
     return { __html: data };
   };
 
-  const optionsList = { 1: 'I Уровень', 2: 'II Уровень', 3: 'III Уровень' };
+  const changeLevel = (lvl) => {
+    history.replace({ search: `?level=${lvl}` });
+    setLevel(lvl);
+    dispatch(fetchMaze({ lvl }));
+  };
 
   useEffect(() => {
-    dispatch(fetchMaze({ lvl }));
-  }, [dispatch, lvl]);
+    const queryLevel = query.get('level');
+    const lvl = Object.keys(optionsList).includes(queryLevel) ? queryLevel : initLevel;
+    changeLevel(lvl);
+
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <div className="maze-map">
-      <Select className="maze-map__field" value={lvl} onChange={setLevel} options={optionsList} />
+      <Select className="maze-map__field" value={lvl} onChange={changeLevel} options={optionsList} />
       <div className="maze-map__content">
         <Loader loading={loading} className="maze-map__loader" />
-        <div dangerouslySetInnerHTML={createMaze()}></div>
+        <div dangerouslySetInnerHTML={getMazeHtml()}></div>
       </div>
     </div>
   );
