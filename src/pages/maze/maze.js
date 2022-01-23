7a1bccc9529bff6af34a25a2df3febab7c864e01
Vadim Novikov <ijcnvv@gmail.com>
2022-01-23 16:29:10 +0300

    adds time to next mazemap updating

--- src/pages/maze/maze.js
+++ src/pages/maze/maze.js
@@ -17,6 +17,12 @@ const MazePage = () => {
   const [lvl, setLevel] = useState(initLevel);
   const query = useMemo(() => new URLSearchParams(search), [search]);
 
+  const date = new Date();
+  const hour = date.getUTCHours(); // 6 14 22
+  const nextUpdateHour = (hour < 6 || hour >= 22 ? 6 : hour < 14 ? 14 : 22) + 3;
+  const nextHour = nextUpdateHour > 24 ? nextUpdateHour - 24 : nextUpdateHour;
+  const nextDate = `${String(nextHour).padStart(2, '0')}:00`;
+
   const getMazeHtml = () => {
     return { __html: data };
   };
@@ -41,6 +47,7 @@ const MazePage = () => {
       <div className="maze-map__content">
         <Loader loading={loading} className="maze-map__loader" />
         <div dangerouslySetInnerHTML={getMazeHtml()}></div>
+        <div className="maze-map__update">Следующее обновление в {nextDate}</div>
       </div>
     </div>
   );
