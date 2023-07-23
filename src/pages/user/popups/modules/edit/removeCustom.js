edea14f0e6e4b18b92e06d18f768a58e51eaeb0d
Vadim Novikov <ijcnvv@gmail.com>
2023-07-23 17:18:33 +0300

    add RemoveCustomCashValue file

---
+++ src/pages/user/popups/modules/edit/removeCustom.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+import PopupForm from "../form";
+
+const RemoveCustomCashValue = ({ setPath, onChange }) => {
+  return (
+    <PopupForm
+      className="wrapper"
+      onBack={() => setPath("remove")}
+      onChange={(count) => onChange("remove", count)}
+      title="Вычесть"
+    />
+  );
+};
+
+RemoveCustomCashValue.propTypes = {
+  setPath: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default RemoveCustomCashValue;
