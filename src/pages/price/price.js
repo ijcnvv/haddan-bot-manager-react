350ed027022bf707ab4f176a319929f65f414b51
Vadim Novikov <ijcnvv@gmail.com>
2023-08-06 11:27:57 +0300

    Update price.js

--- src/pages/price/price.js
+++ src/pages/price/price.js
@@ -4,7 +4,7 @@ const PricePage = () => {
   return (
     <>
       <div className="section">
-        По стоимости и вариантам оплаты вы можете уточнить у <a href="https://t.me/ijcnvv" target="_blank">разработчика</a>
+        По стоимости и вариантам оплаты вы можете уточнить у <a href="https://t.me/ijcnvv" target="_blank" rel="noopener noreferrer">разработчика</a>
        </div>
     </>
   );
