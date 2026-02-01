adf4952752c7e7b25b89db72aebb2b5096c10b97
Vadim Novikov <ijcnvv@gmail.com>
2026-02-01 22:41:18 +0300

    Update listItem.js

--- src/pages/users/modules/listItem.js
+++ src/pages/users/modules/listItem.js
@@ -12,7 +12,6 @@ const User = ({ user }) => {
 
   return (
     <div className="list-item" onClick={clickHandler}>
-      <img className="list-item__icon" src={`/images/${user.network}.png`} alt="icon" />
       <div className="list-item__title">
         <div className="list-item__name">{user.name}</div>
         {user.email ? <div className="list-item__email">({user.email})</div> : null}
