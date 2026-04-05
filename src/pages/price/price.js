721591b97a6123293aabe3cf3b6c0f82328f6143
Vadim Novikov <ijcnvv@gmail.com>
2026-04-05 22:13:01 +0300

    Update price.js

--- src/pages/price/price.js
+++ src/pages/price/price.js
@@ -4,7 +4,8 @@ const PricePage = () => {
   return (
     <>
       <div className="section">
-        Всю необходимую информацию по подключению и оплате вы можете найти в телеграм боте <a href="https://t.me/haddan_jc_bot" target="_blank" rel="noopener noreferrer">@haddan_jc_bot</a>. После подключения вам будет доступен бесплатный период несколько дней, чтобы вы могли полноценно опробовать весь функционал
+        Всю необходимую информацию по подключению и оплате вы можете найти в телеграм боте <a href="https://t.me/haddan_jc_bot" target="_blank" rel="noopener noreferrer">@haddan_jc_bot</a>. После подключения вам будет доступен бесплатный период несколько дней, чтобы вы могли полноценно опробовать весь функционал.<br/><br/>
+        Доступные способы оплаты <a href="https://t.me/wallet" target="_blank" rel="noopener noreferrer">@wallet</a>, <a href="https://yoomoney.ru/to/410014431917625" target="_blank" rel="noopener noreferrer">yoomoney</a>
        </div>
     </>
   );
